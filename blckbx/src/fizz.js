ab769cb16109d265f310e312a58bdc35c1a9f1fe
MohammedSaffath <saffath1011@gmail.com>
2023-07-28 20:27:23 +0100

    UI changes

--- blckbx/src/fizz.js
+++ blckbx/src/fizz.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'; 
 
 const FizzBuzz = () => {
   const [numbers, setNumbers] = useState([]);
@@ -12,13 +13,15 @@ const FizzBuzz = () => {
 
   return (
     <div className="container">
-      <h1>FizzBuzz </h1>
-      <ul>
-        {numbers.map((number, index) => (
-          <li key={index}>{number}</li>
-        ))}
-      </ul>
-    </div>
+    <h1 className="title">FizzBuzz by Mohammed Saffath</h1>
+    <ul className="fizzbuzz-list">
+      {numbers.map((number, index) => (
+        <li key={index} className="fizzbuzz-item">
+          {number}
+        </li>
+      ))}
+    </ul>
+  </div>
   );
 };
 
